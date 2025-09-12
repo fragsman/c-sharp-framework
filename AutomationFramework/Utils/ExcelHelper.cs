50a8bc0d0ef9d4d957a182424fb45f37ad9a37e2
Federico Pantaleone <fragsman@gmail.com>
2025-09-12 17:07:55 -0300

    unsaved changes

--- AutomationFramework/Utils/ExcelHelper.cs
+++ AutomationFramework/Utils/ExcelHelper.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AutomationFramework.Utils
 {
-    internal class ExcelUtil
+    internal class ExcelHelper
     {
     }
 }
--- AutomationFrameworkTest/Support/HookInitialize.cs
+++ AutomationFrameworkTest/Support/HookInitialize.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AutomationFrameworkTest
+namespace AutomationFrameworkTest.Support
 {
     internal class HookInitialize
     {
