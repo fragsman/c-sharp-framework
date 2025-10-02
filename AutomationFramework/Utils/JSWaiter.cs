e97f4f125c1ab5a67185a539ac4f72202f433ea3
Federico Martin Pantaleone <federico.pantaleone@motivus.com>
2025-10-02 15:14:13 -0300

    added utility helpers and methods to interactor

---
+++ AutomationFramework/Utils/JSWaiter.cs
@@ -0,0 +1,165 @@
+﻿using AutomationFramework.Utils;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+public class JSWaiter
+{
+    private static void AjaxComplete(IWebDriver driver)
+    {
+        IJavaScriptExecutor jsExec = (IJavaScriptExecutor)driver;
+        jsExec.ExecuteScript("var callback = arguments[arguments.length - 1];"
+            + "var xhr = new XMLHttpRequest();" + "xhr.open('GET', '/Ajax_call', true);"
+            + "xhr.onreadystatechange = function() {" + "  if (xhr.readyState == 4) {"
+            + "    callback(xhr.responseText);" + "  }" + "};" + "xhr.send();");
+    }
+
+    private static void WaitForJQueryLoad(IWebDriver driver)
+    {
+        IJavaScriptExecutor jsExec = (IJavaScriptExecutor)driver;
+
+        try
+        {
+            Boolean jqueryReady = (Boolean)jsExec.ExecuteScript("return jQuery.active==0");
+            if (!jqueryReady)
+            {
+                WebDriverWait Wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+                Wait.Until(wd => ((IJavaScriptExecutor)wd).ExecuteScript("return jQuery.active==0"));
+            }
+        }
+        catch (WebDriverException e)
+        {
+            Logger.Debug("WaitForJQueryLoad: " + e.Message);
+        }
+    }
+
+    private static void WaitUntilJQueryReady(IWebDriver driver)
+    {
+        IJavaScriptExecutor jsExec = (IJavaScriptExecutor)driver;
+        Boolean jQueryDefined = (Boolean)jsExec.ExecuteScript("return typeof jQuery != 'undefined'");
+        if (jQueryDefined)
+        {
+            Poll(20);
+            WaitForJQueryLoad(driver);
+            Poll(20);
+        }
+    }
+
+    private static void WaitUntilJSReady(IWebDriver driver)
+    {
+        IJavaScriptExecutor jsExec = (IJavaScriptExecutor)driver;
+        try
+        {
+            Boolean jsReady = jsExec.ExecuteScript("return document.readyState").ToString().Equals("complete");
+
+            if (!jsReady)
+            {
+                WebDriverWait Wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+                Wait.Until(wd => ((IJavaScriptExecutor)wd).ExecuteScript("return document.readyState").Equals("complete"));
+            }
+        }
+        catch (WebDriverException e)
+        {
+            Logger.Debug("WaitUntilJSReady: " + e.Message);
+        }
+    }
+    
+    private static void WaitUntilAngular5Ready(IWebDriver driver)
+    {
+        IJavaScriptExecutor jsExec = (IJavaScriptExecutor)driver;
+        try
+        {
+            Object angular5Check = jsExec.ExecuteScript("return getAllAngularRootElements()[0].attributes['ng-version']");
+            if (angular5Check != null)
+            {
+                Boolean angularPageLoaded = (Boolean)jsExec.ExecuteScript("return window.getAllAngularTestabilities().findIndex(x=>!x.isStable()) === -1");
+ 
+                if (!angularPageLoaded)
+                {
+                    Poll(20);
+                    WaitForAngular5Load(driver);
+                    Poll(20);
+                }
+            }
+        }
+        catch (WebDriverException e)
+        {
+            Logger.Debug("WaitUntilAngular5Ready: " + e.Message);
+        }
+    }
+    
+    private static void WaitForAngular5Load(IWebDriver driver)
+    {
+        string angularReadyScript = "return window.getAllAngularTestabilities().findIndex(x=>!x.isStable()) === -1";
+        AngularLoads(driver, angularReadyScript);
+    }
+
+    private static void AngularLoads(IWebDriver driver, string angularReadyScript)
+    {
+        IJavaScriptExecutor jsExec = (IJavaScriptExecutor)driver;
+        try
+        {
+            bool angularReady = Boolean.Parse(jsExec.ExecuteScript(angularReadyScript).ToString());
+            if (!angularReady)
+            {
+                WebDriverWait Wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+                Wait.Until(wd => ((IJavaScriptExecutor)wd).ExecuteScript(angularReadyScript));
+            }
+        }
+        catch (WebDriverException e)
+        {
+            Logger.Debug("AngularLoads: " + e.Message);
+        }
+    }
+
+    public static void WaitAllRequest(IWebDriver driver)
+    {
+        WaitUntilJSReady(driver);
+        AjaxComplete(driver);
+        WaitUntilJQueryReady(driver);
+        WaitUntilAngular5Ready(driver);
+        //WaitUntilAngularReady(); //For oldest Angular prior version 5
+    }
+
+    private static void Poll(int milis)
+    {
+        try
+        {
+            Thread.Sleep(milis);
+        }
+        catch (Exception e)
+        {
+            Logger.Debug(e.Message);
+        }
+    }
+
+    /*
+    private static void WaitForAngularLoad()
+    {
+        String angularReadyScript = "return angular.element(document).injector().get('$http').pendingRequests.length === 0";
+        AngularLoads(angularReadyScript);
+    }
+    
+    private static void WaitUntilAngularReady()
+    {
+        IJavaScriptExecutor jsExec = (IJavaScriptExecutor)DriverContext.Driver;
+        try
+        {
+            Boolean angularUnDefined = (Boolean)jsExec.ExecuteScript("return window.angular === undefined");
+            if (!angularUnDefined)
+            {
+                Boolean angularInjectorUnDefined = (Boolean)jsExec.ExecuteScript("return angular.element(document).injector() === undefined");
+                if (!angularInjectorUnDefined)
+                {
+                    Poll(20);
+                    WaitForAngularLoad();
+                    Poll(20);
+                }
+            }
+        }
+        catch (WebDriverException e)
+        {
+            Console.WriteLine("WaitUntilAngularReady: " + e.Message);
+        }
+    }
+    */
+}
---
+++ AutomationFramework/Utils/StringHelper.cs
@@ -0,0 +1,110 @@
+﻿using System.Text;
+
+namespace AutomationFramework.Helpers
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Returns a string of 'length' alphabetic characters. The range goes from from 0 to N, where N is the max number 
+        /// according to the number of the digits given by length parameter. If 'length' is 4 then N will be 9999.
+        /// <param name="length">length of the resulting string</param>
+        /// </summary>
+        public static string GetRandomStringOfNumbers(int length)
+        {
+            Random rnd = new Random();
+            int topNumber = GetMaxNumberForDigits(length);
+            return rnd.Next(0, topNumber).ToString();
+        }
+
+        /// <summary>
+        /// Returns a string of 'length' numeric characters
+        /// - Use the Random.NextDouble() method to generate a float (flt) that is between 0.0 and 1.0 and is inclusive.
+        /// - Multiply flt with 25 and take the Floor of the result. 
+        /// This will return an integer(shift) that is between 0 and 25 and is inclusive.
+        /// - Add the shift integer to 65, which is the ASCII value of the character A.
+        /// This will return an inclusive value between 65 and 90, which will be the ASCII value of some character.
+        /// Converting that value to a character will return an uppercase character.
+        /// </summary>
+        /// <param name="length">length of the resulting string</param>
+        /// <returns></returns>
+        public static string GetRandomStringOfLetters(int length)
+        {
+            StringBuilder tempString = new StringBuilder();
+            Random rnd = new Random();
+
+            char letter;
+            for (int i = 0; i < length; i++)
+            {
+                double flt = rnd.NextDouble();
+                int shift = Convert.ToInt32(Math.Floor(25 * flt));
+                letter = Convert.ToChar(shift + 65);
+                tempString.Append(letter);
+            }
+            return tempString.ToString();
+        }
+
+        private static int GetMaxNumberForDigits(int length)
+        {
+            string tempNumber = "";
+            for (int i = 0; i < length; i++)
+            {
+                tempNumber += "9";
+            }
+            int finalNumber = int.Parse(tempNumber);
+            return finalNumber;
+        }
+
+        public static string ConvertToMoneyFormat(string raw_number)
+        {
+            //Output format $000.000 (entire part only)
+            //1     1               $1
+            //2     10              $10
+            //3     100             $100
+            //4     1000            $1,000
+            //5     10000           $10,000
+            //6     100000          $100,000
+            //7     1000000         $1,000,000
+            //8     10000000        $10,000,000
+            //9     100000000       $100,000,000
+            //10    1000000000      $1,000,000,000
+            //11    10000000000     $10,000,000,000
+            //12    100000000000    $100,000,000,000
+            string result;
+            string number;
+            string symbol = "";
+            if (raw_number[0].Equals('-')) //is negative
+            {
+                number = raw_number.Substring(1, raw_number.Length-1);
+                symbol = "-";
+            }
+            else //is positive (no symbol expected)
+            {
+                number = raw_number;
+            }
+            int len = number.Length;
+            switch (len)
+            {
+                case 1: break;
+                case 2: break;
+                case 3: break;
+                case 4: number = number.Insert(1, ","); break;
+                case 5: number = number.Insert(2, ","); break;
+                case 6: number = number.Insert(3, ","); break;
+                case 7: number = number.Insert(4, ","); number = number.Insert(1, ","); break;
+                case 8: number = number.Insert(5, ","); number = number.Insert(2, ","); break;
+                case 9: number = number.Insert(6, ","); number = number.Insert(3, ","); break;
+                case 10: number = number.Insert(7, ","); number = number.Insert(4, ","); number = number.Insert(1, ","); break;
+                case 11: number = number.Insert(8, ","); number = number.Insert(5, ","); number = number.Insert(2, ","); break;
+                case 12: number = number.Insert(9, ","); number = number.Insert(6, ","); number = number.Insert(3, ","); break;
+            }
+            result = symbol + "$" + number;
+            return result;
+        }
+
+        //Converts a date to what is shown on WebSite screen. Currently the format is dd-MMM-yyyy
+        public static string ConvertDateToOnScreenDateFormat(DateTime date)
+        {
+            return date.ToString("dd-MMM-yyyy");
+        }
+    }
+}
